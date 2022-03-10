393d1d6d3d97b077ece72c706d70b8ddbc9479d5
ztaj0118 <84503440+ztaj0118@users.noreply.github.com>
2022-03-10 11:23:51 +0530

    Update synch.sh

--- synch.sh
+++ synch.sh
@@ -1,1 +1,15 @@
 echo "send file"
+#!/bin/bash
+export HOME=/tmp # so Git can write .gitconfig here
+CLONE_DIR=/tmp/src-repo
+sudo chmod -R 777 /tmp/src-repo
+DEST_REPO='https://aj0118+1-at-547321245783:fjJvzOFnk0fM02pV0NMJLH7sIRALPh3FXpFbQ8EBWYs=@git-codecommit.ap-south-1.amazonaws.com/v1/repos/sync'
+SRC_REPO='https://ztaj0118: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@github.com/ztaj0118/sync.git'
+
+rm -rf $CLONE_DIR
+mkdir $CLONE_DIR
+git clone --mirror "$SRC_REPO" $CLONE_DIR
+cd $CLONE_DIR
+git remote add dest "$DEST_REPO"
+git push dest --mirror
+echo 'DONE! Successfully mirrored source repo to destination!'
