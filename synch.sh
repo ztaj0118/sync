5cc902f930920dbe6e3eb0aaff4b2b36898e0037
ztaj0118 <84503440+ztaj0118@users.noreply.github.com>
2022-03-10 11:38:36 +0530

    Update synch.sh

--- synch.sh
+++ synch.sh
@@ -4,7 +4,7 @@ export HOME=/tmp # so Git can write .gitconfig here
 CLONE_DIR=/tmp/src-repo
 sudo chmod -R 777 /tmp/src-repo
 DEST_REPO='https://aj0118+1-at-547321245783:fjJvzOFnk0fM02pV0NMJLH7sIRALPh3FXpFbQ8EBWYs=@git-codecommit.ap-south-1.amazonaws.com/v1/repos/sync'
-SRC_REPO='https://ztaj0118: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@github.com/ztaj0118/sync.git'
+SRC_REPO='https://ztaj0118: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@github.com/ztaj0118/sync.git'
 
 rm -rf $CLONE_DIR
 mkdir $CLONE_DIR
